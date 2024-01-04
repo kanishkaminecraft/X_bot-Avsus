17c39f9203e634f5563d13e5eb12d07d8d270a08
kanishkaminecraft <113976869+kanishkaminecraft@users.noreply.github.com>
2024-01-04 22:36:52 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
-clear
-echo -e "\033[34m
-                        _       ___     ______            
-                       | |     / \ \   / / ___| _   _ ___ 
-                      / __)   / _ \ \ / /\___ \| | | / __|
-                      \__ \  / ___ \ V /  ___) | |_| \__INSTALLER
-                      (   / /_/   \_\_/  |____/ \__,_|___/
-                       |_|
-\033[0m"
+m="\e[0;31m"   # red
+k="\e[0;33m"   # yellow
+h="\e[0;32m"   # green
+b="\e[0;34m"   # blue
+lm="\e[1;31m"  # pink
+lk="\e[1;33m"  # bright yellow
+lh="\e[1;32m"  # light green
+lb="\e[1;34m"  # blue sky
+n="\e[0m"      # neutral
+w="\e[1;37m"   #thick white
 
-echo -e "\033[31m
-[+] INSTALLING...
-\033[0m"
+clear
+echo -e "
+$h
+             |	  ____   _    _ ____      ____   ____  ____
+           __|__ |____|   \  / |____     |____  |____ |
+          |__|_	 |    |    \/   ____|     ____| |____ |____ . B/W
+          ___|_|
+             | INSTALLER .SH
+             | $h
+"
+
+echo -e "$m [ + ] INSTALLING ADB ...$m "
 sudo apt install adb
-sudo apt intall snap
-sudo apt install nmap
-sudo apt install lolcat
+echo ""
+echo -e "$lm [ + ] INSTALLING RUBY$lm"
+sudo apt install ruby -y
+echo ""
+echo -e "$b[ + ] INSTALING LOLCAT$b"
+gem install lolcat
+echo ""
+echo -e "$h[ + ] INSTALLING FESTIVAL TOOL$h"
 sudo apt install festival
-echo -e "\033[31m
-[+] INSTALLATION IS COMPLETED...
-\033[0m"
-echo -e "\033[31m
-[+] COMPLETELY INSTALLED $ AVSUS'S TOOL YOU MAY EXIT INSTALLER...
-\033[0m"
-chmod +x X.sh
+echo ""
+echo ""
+echo "[+] ALL DATA IS INSTALLED KINDLE CLOSE THE INSTALLER AND RUN THE ACTUAL CODE :-)"
